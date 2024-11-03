725fe0d2c3dbb446100e5971be5484de8dd64b30
= <=>
2024-11-03 13:02:10 -0500

    Handle errors with .catch() and display error message.

--- main.js
+++ main.js
@@ -3,35 +3,41 @@
 const productContainer = document.getElementById('product-container');
 const errorMessage = document.getElementById('error-message');
 
-// Task 2: 
-fetch('https://www.course-api.com/javascript-store-products')
+// Task 3: Display Product Details dynamically 
+
+function displayProducts(products) { // Creating function to display products
+    products.forEach(product => {
+        const productDiv = document.createElement('div'); 
+        productDiv.classList.add('product');
+
+        // Displayinh product information in the appropriate div
+        productDiv.innerHTML =  ` 
+            <h2>${product.name}</h2>
+            <h3>${product.company}</h3>
+            <p>Price: $${product.price}</p>
+            <img src="${product.image}"/>
+        `;
+
+        productContainer.appendChild(productDiv); // Appending div
+    });
+}
+
+// Task 2: Fetch Products from the API Using Fetch and Promises
+fetch('https://www.course-api.com/javascript-store-products') // Fetch data from the API
     .then(response => {
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok'); // Check if response is ok and display error if not
         }
-        return response.json();
+        return response.json(); // Parse the JSON from the response
     })
     .then(data => {
-        displayProducts(data);
+        displayProducts(data); // Call the function displayProducts from task 3
     })
+    // Task 4: Handle errors gracefully
     .catch(error => {
         errorMessage.textContent = "Failed to load products. Please try again later.";
         errorMessage.style.display = 'block';
         console.error('There was a problem with the fetch operation:', error);
     });
 
-    function displayProducts(products) {
-        products.forEach(product => {
-            const productDiv = document.createElement('div');
-            productDiv.classList.add('product');
-    
-            productDiv.innerHTML = `
-                <h2>${product.name}</h2>
-                <h3>${product.company}</h3>
-                <p>Price: $${product.price}</p>
-                <img src="${product.image}"/>
-            `;
-    
-            productContainer.appendChild(productDiv);
-        });
-    }
+   
