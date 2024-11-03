29190087c5f04423df493798c11ed8a77b2acc5c
= <=>
2024-11-03 13:19:11 -0500

    Final commit

--- main.js
+++ main.js
@@ -3,25 +3,6 @@
 const productContainer = document.getElementById('product-container');
 const errorMessage = document.getElementById('error-message');
 
-// Task 3: Display Product Details dynamically 
-
-function displayProducts(products) { // Creating function to display products
-    products.forEach(product => {
-        const productDiv = document.createElement('div'); 
-        productDiv.classList.add('product');
-
-        // Displayinh product information in the appropriate div
-        productDiv.innerHTML =  ` 
-            <h2>${product.name}</h2>
-            <h3>${product.company}</h3>
-            <p>Price: $${product.price}</p>
-            <img src="${product.image}"/>
-        `;
-
-        productContainer.appendChild(productDiv); // Appending div
-    });
-}
-
 // Task 2: Fetch Products from the API Using Fetch and Promises
 fetch('https://www.course-api.com/javascript-store-products') // Fetch data from the API
     .then(response => {
@@ -31,8 +12,10 @@ fetch('https://www.course-api.com/javascript-store-products') // Fetch data from
         return response.json(); // Parse the JSON from the response
     })
     .then(data => {
+        console.log(data);
         displayProducts(data); // Call the function displayProducts from task 3
     })
+
     // Task 4: Handle errors gracefully
     .catch(error => {
         errorMessage.textContent = "Failed to load products. Please try again later.";
@@ -40,4 +23,24 @@ fetch('https://www.course-api.com/javascript-store-products') // Fetch data from
         console.error('There was a problem with the fetch operation:', error);
     });
 
+    // Task 3: Display Product Details dynamically 
+
+function displayProducts(data) { // Creating function to display products
+    const products = data.products;
+    data.forEach(item => {
+        const product = item.fields
+        const productDiv = document.createElement('div'); 
+        productDiv.classList.add('product');
+
+        // Displaying product information in the appropriate div
+        productDiv.innerHTML =  ` 
+            <h2>${product.name}</h2>
+            <h3>${product.company}</h3>
+            <p>Price: $${product.price}</p>
+            <img src="${product.image[0].url}"/>
+        `;
+
+        productContainer.appendChild(productDiv); // Appending div to the container
+    });
+}
    
