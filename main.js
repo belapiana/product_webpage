29436bdede93cee4e7d8460fd9fd8195254c4d19
= <=>
2024-11-03 12:56:06 -0500

    Format product information dynamically

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// Setting up constants
+
 const productContainer = document.getElementById('product-container');
 const errorMessage = document.getElementById('error-message');
 
+// Task 2: 
 fetch('https://www.course-api.com/javascript-store-products')
     .then(response => {
         if (!response.ok) {
@@ -16,3 +19,19 @@ fetch('https://www.course-api.com/javascript-store-products')
         errorMessage.style.display = 'block';
         console.error('There was a problem with the fetch operation:', error);
     });
+
+    function displayProducts(products) {
+        products.forEach(product => {
+            const productDiv = document.createElement('div');
+            productDiv.classList.add('product');
+    
+            productDiv.innerHTML = `
+                <h2>${product.name}</h2>
+                <h3>${product.company}</h3>
+                <p>Price: $${product.price}</p>
+                <img src="${product.image}"/>
+            `;
+    
+            productContainer.appendChild(productDiv);
+        });
+    }
